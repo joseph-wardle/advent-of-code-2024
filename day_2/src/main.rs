9131cde667cc58a52e770341bdeedca028572d3d
joseph-wardle <joseph.m.wardle@gmail.com>
2025-08-08 00:24:40 +0300

    day 2 part 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -18,20 +18,44 @@ fn part_1(input: &str) -> u32 {
                 .map(|num| num.parse().unwrap())
                 .collect();
 
-            let diffs_ok = report
-                .windows(2)
-                .all(|w| (1..=3).contains(&w[0].abs_diff(w[1])));
-            let increasing = report.windows(2).all(|w| w[0] < w[1]);
-            let decreasing = report.windows(2).all(|w| w[0] > w[1]);
-
-            let is_safe = diffs_ok && (increasing || decreasing);
-            is_safe as u32
+            is_safe(&report) as u32
         })
         .sum()
 }
 
-fn part_2(_input: &str) -> u32 {
-    0
+fn part_2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let report: Vec<u32> = line
+                .split_whitespace()
+                .map(|num| num.parse().unwrap())
+                .collect();
+
+            if is_safe(&report) {
+                1
+            } else {
+                for i in 0..report.len() {
+                    let mut temp = report.clone();
+                    temp.remove(i);
+                    if is_safe(&temp) {
+                        return 1;
+                    }
+                }
+                0
+            }
+        })
+        .sum()
+}
+
+fn is_safe(report: &[u32]) -> bool {
+    let diffs_ok = report
+        .windows(2)
+        .all(|w| (1..=3).contains(&w[0].abs_diff(w[1])));
+    let increasing = report.windows(2).all(|w| w[0] < w[1]);
+    let decreasing = report.windows(2).all(|w| w[0] > w[1]);
+
+    diffs_ok && (increasing || decreasing)
 }
 
 #[cfg(test)]
@@ -50,6 +74,6 @@ mod tests {
     fn test_part_2() {
         let input = fs::read_to_string("data/example.txt").unwrap();
         let result = part_2(&input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 4);
     }
 }
